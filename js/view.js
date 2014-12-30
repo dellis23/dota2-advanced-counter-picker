d282c83316b359acc5cf33011cdfa45bee8ff86a
Dan Ellis <dan.ellis@saucelabs.com>
2014-12-30 14:26:44 -0800

    Binding and display of counters.

--- js/view.js
+++ js/view.js
@@ -2,13 +2,29 @@ var selected_abilities = [];
 
 var counter_abilities = [];
 
-var available_abilities = abilities; 
+var available_abilities = abilities;
 
 function select_ability(ability_node) {
     selected_abilities.push(
         get_ability(
             abilities, ability_node.attr("hero"), ability_node.attr("name")
         )
+    );
+}
+
+function recalculate_counters() {
+    counter_abilities.length = 0;
+    _.each(
+        _.flatten(
+            _.map(
+                selected_abilities,
+                _.partial(
+                    get_abilities_countering_ability, counters, abilities
+                )
+            )
+        ), function(ability) {
+            counter_abilities.push(ability);
+        }
     );
 }
 
@@ -28,12 +44,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     rivets.bind(
         $('#available_abilities'), {
-            available_abilities: available_abilities 
+            available_abilities: available_abilities
         }
     );
 
     $("#available_abilities .ability").on("click", function() {
         select_ability($(this));
+        recalculate_counters();
     });
 
 });
